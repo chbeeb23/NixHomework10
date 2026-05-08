0d402fe48865700587c0411a3a57e38e61becbf5
mperepelitsa23 <chbeeb23@gmail.com>
2026-05-09 00:34:56 +0300

    NixHomework 10 done

---
+++ Source/NixHomework10/NixHomework10.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NixHomework10.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NixHomework10, "NixHomework10" );
